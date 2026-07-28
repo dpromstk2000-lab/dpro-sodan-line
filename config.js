ade044d00e7d2f362110afd51f31b99965521c50
dpromstk2000-lab <dpromstk2000@gmail.com>
2026-07-28 15:11:20 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 window.DPRO_CONFIG = Object.freeze({
   systemCode: "SODAN",
   appName: "DPRO 相談支援事業所 LINE",
-  version: "SODAN-3-WEB-20260728",
+  version: "SODAN-4-WEB-20260728",
   environment: "demo",
   officeCode: "dpro_sodan_demo",
   workerUrl: "https://dpro-sodan-line-api.dpromstk2000.workers.dev",
   liffId: "",
+  memberPath: "member.html",
   timezone: "Asia/Tokyo"
 });
